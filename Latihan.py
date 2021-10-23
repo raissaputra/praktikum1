7742fd4d506e62c5cd3c0b4c9beea62acb02f090
raissaputra <raissaputra02@gmail.com>
2021-10-23 10:30:45 +0700

    Tugas pertemuan 05 (Kerjakan Latihan)

---
+++ Latihan.py
@@ -0,0 +1,15 @@
+nama_lengkap = "Rais Saputra"
+nama_panggilan = "Rais"
+npm = 311810195
+umur = 25
+tempat_lahir = "Kebumen"
+telepon = 6281549454938
+alamat = "Harapan Mulya NO.20B, Medan satria, Kota Bekasi"
+
+print("="*30)
+
+txt = "Assalamu'alaikum.\n\nLet me introduce my self. My name is {}, but you can call me {}. My NPM is {}. I was born in {} and I am {} years old. I am very glad if you want to invite my house in {}. So, don't forget to call me before with the number {}.\n\nThank you."
+
+print(txt.format(nama_lengkap, nama_panggilan,
+      npm, tempat_lahir, umur, alamat, telepon))
+print("="*30)
