ab61071519f8264d61ab1b5b722d57ad4e119579
raissaputra <raissaputra02@gmail.com>
2021-10-25 20:15:42 +0700

    tambah file latihan1.py dan output program

---
+++ latihan1.py
@@ -0,0 +1,23 @@
+print("hello")
+print("Saya sedang belajar python")
+
+a = 8
+b = 6
+print("variable a = ", a)
+print("variable b = ", b)
+print("Hasil penjumlahan a+b = ", a+b)
+
+a = input("masukan nilai a : ")
+b = input("masukan nilai b : ")
+
+# a = int(input("masukan nilai a : "))
+# b = int(input("masukan nilai b : "))
+print("variable a = ", a)
+print("variable b = ", b)
+print("Hasil penggabungan {1}&{0}=%d".format(a, b) % (a+b))
+
+# konversi nilai variable
+a = int(a)
+b = int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a, b) % (a+b))
+print("hasil pembagian {1}/{0}=%d".format(a, b) % (a/b))
