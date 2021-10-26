965ee9769305f95b60d74cdbbac3f8a3f82177f0
raissaputra <raissaputra02@gmail.com>
2021-10-26 21:00:52 +0700

    edit program agar bisa berjalan normal

--- latihan1.py
+++ latihan1.py
@@ -10,14 +10,14 @@ print("Hasil penjumlahan a+b = ", a+b)
 a = input("masukan nilai a : ")
 b = input("masukan nilai b : ")
 
-# a = int(input("masukan nilai a : "))
-# b = int(input("masukan nilai b : "))
+#a = int(input("masukan nilai a : "))
+#b = int(input("masukan nilai b : "))
 print("variable a = ", a)
 print("variable b = ", b)
-print("Hasil penggabungan {1}&{0}=%d".format(a, b) % (a+b))
+print("Hasil penggabungan {1}&{0}=%s".format(a, b) % (a+b))
 
 # konversi nilai variable
 a = int(a)
 b = int(b)
 print("hasil penjumlahan {1}+{0}=%d".format(a, b) % (a+b))
-print("hasil pembagian {1}/{0}=%d".format(a, b) % (a/b))
+print("hasil pembagian {1}/{0}=%f".format(a, b) % (a/b))
